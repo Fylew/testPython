e73e3f9818027c1abddb6d94a3be78f0257e6ba0
Dreamer <skilet.e@yandex.ru>
2022-10-19 22:23:41 +0300

    in progress

--- Game/1.py
+++ Game/1.py
@@ -17,14 +17,71 @@ for alfa in alf[0]:
 for i in word:
     i=i[:-1]
     dikt[i[0]].append(i)
+def medium_game(word_in_game,ww,worde): #the average level of the game
 
-def izi_game(word_in_game,ww): #easy level of play
     global w
     global word_counts
+
+    worde.delete(0, 'end')
+    label1['text'] = "Простой режим\n" \
+                     "нужно назвать {} слов(а) для победы!".format(49 - word_counts)
+
+    if word_counts == 50:
+        label1['text'] = "Вы победили!!!"
+        exit()
+
+    tail_word = game(word_in_game,ww)
+
+    wordes(tail_word,ww)
+
+    word_counts += 1
+
+
+    medi()
+
+def Hard_game(word_in_game,ww,worde): #challenging game level
+    global w
+    global word_counts
+
+    worde.delete(0, 'end')
+    label1['text'] = "Простой режим\n" \
+                     "нужно назвать {} слов(а) для победы!".format(99 - word_counts)
+
+    if word_counts == 100:
+        label1['text'] = "Вы победили!!!"
+        exit()
+
+    tail_word = game(word_in_game,ww)
+
+    wordes(tail_word,ww)
+
+    word_counts += 1
+    hard()
+
+def unreal_game(word_in_game,ww,worde): #endless game level
+    global w
+    global word_counts
+
+    worde.delete(0, 'end')
+
+    label1['text'] = "Безконченый режим\n" \
+                     "названно слов {}".format(word_counts)
+
+    tail_word=game(word_in_game,ww)
+
+    wordes(tail_word,ww)
+    word_counts +=1
+    unr()
+def izi_game(word_in_game,ww,worde): #easy level of play
+    global w
+    global word_counts
+
+    worde.delete(0, 'end')
+
     label1['text'] = "Простой режим\n" \
                      "нужно назвать {} слов(а) для победы!".format(24 - word_counts)
     if word_counts == 25:
-        print("Вы победили!!!")
+        label1['text'] = "Вы победили!!!"
         exit()
 
     tail_word=game(word_in_game,ww)
@@ -56,9 +113,6 @@ def wordes(tail_word,ww): #getting a list of words for the desired letter
     if word in word_out:
         word = random.choice(dikt[tail_word])
 
-    ww["text"] = word
-
-
     if word[-1] == "ъ" or word[-1] == "ы" or word[-1] == 'ь':
         w = word[-2]
         word_out.append(word)
@@ -66,7 +120,7 @@ def wordes(tail_word,ww): #getting a list of words for the desired letter
     else:
         w = word[-1]
         word_out.append(word)
-        print(f"Тебе на {w}")
+        ww["text"] = f"{word}, тебе на {w} "
 
     word_out.append(word)
 
@@ -99,19 +153,74 @@ def game(word,ww): #The main part of the game, with word input
 
 def izi():
     label1['text'] = "Простой режим\n" \
-                     "нужно назвать {} слов для победы!".format(25-word_counts)
+                     "нужно назвать {} слов(а) для победы!".format(25)
+    izi.destroy()
+    medi.destroy()
+    hard.destroy()
+    unr.destroy()
 
-    izi.destroy()# удаляю все конопки с экрана
+
     worde = tk.Entry(win)# добавляю полле ввода
     worde.pack()
     ww = tk.Label(win,text="",font=('Arial',15,"bold"))
     ww.pack() # добавляю пустую строку в дальнейшем для вывода ответов
-    but1 = tk.Button(win, text="Ввод", command=lambda: izi_game(worde.get(),ww)) # создаю кнопку для ввода команд и передачи аргумента
+    but1 = tk.Button(win, text="Ввод", command=lambda: izi_game(worde.get(),ww,worde)) # создаю кнопку для ввода команд и передачи аргумента
+    but1.pack()
+def medi():
+    label1['text'] = "Средний режим\n" \
+                     "нужно назвать {} слов(а) для победы!".format(50)
+    izi.destroy()
+    medi.destroy()
+    hard.destroy()
+    unr.destroy()
+
+
+    worde = tk.Entry(win)# добавляю полле ввода
+    worde.pack()
+    worde.delete(0, 'end')
+    ww = tk.Label(win, text="", font=('Arial', 15, "bold"))
+    ww.pack()  # добавляю пустую строку в дальнейшем для вывода ответов
+    but1 = tk.Button(win, text="Ввод",
+                     command=lambda: medium_game(worde.get(), ww,worde))  # создаю кнопку для ввода команд и передачи аргумента
+    but1.pack()
+
+def hard():
+    label1['text'] = "Сложный режим\n" \
+                     "нужно назвать {} слов(а) для победы!".format(100)
+
+    izi.destroy()
+    medi.destroy()
+    hard.destroy()
+    unr.destroy()
+
+
+    worde = tk.Entry(win)# добавляю полле ввода
+    worde.pack()
+    ww = tk.Label(win, text="", font=('Arial', 15, "bold"))
+    ww.pack()  # добавляю пустую строку в дальнейшем для вывода ответов
+    but1 = tk.Button(win, text="Ввод",
+                     command=lambda: Hard_game(worde.get(), ww,worde))  # создаю кнопку для ввода команд и передачи аргумента
+    but1.pack()
+def unr():
+    label1['text'] = "Безконечный режим"
+
+    izi.destroy()
+    medi.destroy()
+    hard.destroy()
+    unr.destroy()
+
+
+    worde = tk.Entry(win)# добавляю полле ввода
+    worde.pack()
+    ww = tk.Label(win, text="", font=('Arial', 15, "bold"))
+    ww.pack()  # добавляю пустую строку в дальнейшем для вывода ответов
+    but1 = tk.Button(win, text="Ввод",
+                     command=lambda: unreal_game(worde.get(), ww,worde))  # создаю кнопку для ввода команд и передачи аргумента
     but1.pack()
 
 win = tk.Tk()
 win.title("Word game v1.0")
-win.geometry("600x500+600+250")
+win.geometry("550x300+600+250")
 
 label1 = tk.Label(win, text="Выбор сложности игры",
                   font=('Arial',20,"bold")
@@ -122,5 +231,13 @@ label1.pack()
 izi = tk.Button(win,text="Простой режим",command=izi)
 izi.pack()
 
+medi = tk.Button(win,text="Средний режим",command=medi)
+medi.pack()
+
+hard = tk.Button(win,text="Сложный режим",command=hard)
+hard.pack()
+
+unr = tk.Button(win,text="Безконечный режим",command=unr)
+unr.pack()
 
 win.mainloop()
